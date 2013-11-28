4aed5cd8e978ee0e4f9b4f140bc42ffd0f0d26e6
antklim <antklim@gmail.com>
2013-11-28 22:48:36 +1000

    A -prevent default directive

--- app/js/directives/directives.js
+++ app/js/directives/directives.js
@@ -7,3 +7,16 @@ RhinoIn.directive('appVersion', ['version', function(version) {
       elm.text(version);
     };
   }]);
+
+RhinoIn.directive('a', function() {
+    return {
+        restrict: 'E',
+        link: function(scope, elem, attrs) {
+            if(attrs.ngClick || attrs.href === '' || attrs.href === '#'){
+                elem.on('click', function(e){
+                    e.preventDefault();
+                });
+            }
+        }
+   };
+});
